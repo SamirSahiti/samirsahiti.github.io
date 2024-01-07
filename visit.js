e1109f16962670360becd8729fb43febf1d30d42
SamirSahiti <155672219+SamirSahiti@users.noreply.github.com>
2024-01-07 14:44:35 +0100

    Add files via upload

---
+++ visit.js
@@ -0,0 +1,92 @@
+
+var checkbox = document.getElementById('ss-no');
+var div = document.getElementById('ss-clickNo');
+
+checkbox.addEventListener('change', function(){
+    if (checkbox.checked) {
+        div.style.display = 'block';
+    } else {
+        div.style.display = 'none';
+    }
+})
+
+var checkbox1 = document.getElementById('ss-yes');
+var div1 = document.getElementById('ss-clickYes');
+
+checkbox1.addEventListener('change', function(){
+    if (checkbox1.checked) {
+        div1.style.display = 'block';
+    } else {
+        div1.style.display = 'none';
+    }
+})
+var checkbox2 = document.getElementById('ss-colossus');
+var div2 = document.getElementById('ss-clickColossus');
+
+checkbox2.addEventListener('change', function(){
+    if (checkbox2.checked) {
+        div2.style.display = 'block';
+    } else {
+        div2.style.display = 'none';
+    }
+})
+var checkbox3 = document.getElementById('ss-garden');
+var div3 = document.getElementById('ss-clickGarden');
+
+checkbox3.addEventListener('change', function(){
+    if (checkbox3.checked) {
+        div3.style.display = 'block';
+    } else {
+        div3.style.display = 'none';
+    }
+})
+var checkbox4 = document.getElementById('ss-lighthouse');
+var div4 = document.getElementById('ss-clickLighthouse');
+
+checkbox4.addEventListener('change', function(){
+    if (checkbox4.checked) {
+        div4.style.display = 'block';
+    } else {
+        div4.style.display = 'none';
+    }
+})
+var checkbox5 = document.getElementById('ss-tomb');
+var div5 = document.getElementById('ss-clickTomb');
+
+checkbox5.addEventListener('change', function(){
+    if (checkbox5.checked) {
+        div5.style.display = 'block';
+    } else {
+        div5.style.display = 'none';
+    }
+})
+var checkbox6 = document.getElementById('ss-pyramid');
+var div6 = document.getElementById('ss-clickPyramid');
+
+checkbox6.addEventListener('change', function(){
+    if (checkbox6.checked) {
+        div6.style.display = 'block';
+    } else {
+        div6.style.display = 'none';
+    }
+})
+var checkbox7 = document.getElementById('ss-statue');
+var div7 = document.getElementById('ss-clickStatue');
+
+checkbox7.addEventListener('change', function(){
+    if (checkbox7.checked) {
+        div7.style.display = 'block';
+    } else {
+        div7.style.display = 'none';
+    }
+})
+var checkbox8 = document.getElementById('ss-temple');
+var div8 = document.getElementById('ss-clickTemple');
+
+checkbox8.addEventListener('change', function(){
+    if (checkbox8.checked) {
+        div8.style.display = 'block';
+    } else {
+        div8.style.display = 'none';
+    }
+})
